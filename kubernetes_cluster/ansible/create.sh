d9e1181ea8c19ed08606fa17478434c73f91236a
Miguel <miguel.delmoral@jonajo.com>
2019-10-30 21:45:46 -0600

    Add ECR secrets configuration

--- kubernetes_cluster/ansible/create.sh
+++ kubernetes_cluster/ansible/create.sh
@@ -2,3 +2,4 @@ ansible-playbook -i hosts kube-dependencies.yml --private-key my-key.pem
 
 ansible-playbook -i hosts master.yml --private-key my-key.pem
 ansible-playbook -i hosts workers.yml --private-key my-key.pem
+ansible-playbook -i hosts aws-credentials.yml --private-key my-key.pem
