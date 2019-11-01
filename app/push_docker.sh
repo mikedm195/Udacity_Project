9a27a1c330995c42dfae9178cd6476f4d733411b
Miguel <miguel.delmoral@jonajo.com>
2019-10-31 19:13:20 -0600

    Minor changes

--- app/push_docker.sh
+++ app/push_docker.sh
@@ -1,4 +1,4 @@
-make build
+# make build
 $(aws ecr get-login --no-include-email --region us-west-2)
 docker tag fibonacci:latest 318180760014.dkr.ecr.us-west-2.amazonaws.com/udacity-project:latest
 docker push 318180760014.dkr.ecr.us-west-2.amazonaws.com/udacity-project:latest
